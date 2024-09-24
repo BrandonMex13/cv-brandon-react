1f801787fc3cfa28d0fb1992919561f987f5011d
Brandon Montoya <brandonmex13@gmail.com>
2024-09-24 00:57:09 -0700

    Fix: se corrigue bug en deploy

--- src/components/Presentacion.tsx
+++ src/components/Presentacion.tsx
@@ -1,17 +1,5 @@
-'use client'
-
-import { useState } from 'react'
-import { Dialog, DialogPanel } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-
-const navigation = [
-    { name: 'Sobre mi', href: '#' },
-    { name: 'Proyectos', href: '#' },
-    { name: 'Conocimientos', href: '#' }
-]
-
 export default function Presentacion() {
-    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
 
     return (
         <div className="bg-white">
