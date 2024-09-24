fcd08a485dece6e80f723991f312dccc8be4ecd0
Brandon Montoya <brandonmex13@gmail.com>
2024-09-24 00:50:16 -0700

    Actualizamos el CV

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import Presentacion from "./components/Presentacion"
 import Proyectos from "./components/Proyectos"
+import Sobremi from "./components/Sobremi"
 import Tecnologias from "./components/Tecnologias"
 
 function App() {
     return (
         <>
             <Presentacion/>
+
+            <Sobremi />
 
             <Proyectos />
 
